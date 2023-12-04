619d55400ef6466ac917ea15612c3f5faf66fe29
IannuccilloAntonio2 <100272555+IannuccilloAntonio2@users.noreply.github.com>
2023-12-04 10:30:40 +0100

    Update example.java

--- example.java
+++ example.java
@@ -1,11 +1,15 @@
-// Get username from parameters
-String username = request.getParameter("username");
+public static void main(String[] args) {
+	   String username = "username"
 // Create a statement from database connection
-Statement statement = connection.createStatement();  
+    Statement statement = connection.createStatement();  
 // Create unsafe query by concatenating user defined data with query string
-String query = "SELECT secret FROM Users WHERE (username = '" + username + "' AND NOT role = 'admin')";
+  String query = "SELECT secret FROM Users WHERE (username = '" + username + "' AND NOT role = 'admin')";
 // ... OR ...
 // Insecurely format the query string using user defined data 
 String query = String.format("SELECT secret FROM Users WHERE (username = '%s' AND NOT role = 'admin')", username);
 // Execute query and return the results
 ResultSet result = statement.executeQuery(query);
+	}
+
+// Get username from parameters
+
